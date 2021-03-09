b7c03961cc089bc9b018d7c279402fca73980b4c
Kaiohenriqueps <kh.silva@hotmail.com>
2021-03-09 09:46:09 -0300

    lendo arquivo em um dataframe

--- csvwrapper/wrapper.go
+++ csvwrapper/wrapper.go
@@ -1,36 +1,31 @@
 package csvwrapper
 
 import (
-	"encoding/csv"
 	"fmt"
 	"os"
+	"github.com/kniren/gota/dataframe"
 )
 
-type exampleData struct {
+
+type exampleta struct {
 	Name string
-	Age  string
+	Age string
 	City string
 }
 
-// OpenFile é uma função que lê um arquivo CSV, com o caminho passado por parâmetro.
+
+// OpenFile é uma função queê um arquivo CSV, com o caminho passado por parâmetro.
 func OpenFile(path string) {
 	csvFile, err := os.Open(path)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("Successfully Opened CSV file")
-	defer csvFile.Close()
-
-	csvLines, err := csv.NewReader(csvFile).ReadAll()
-	if err != nil {
-		fmt.Println(err)
-	}
-	for _, line := range csvLines {
-		emp := exampleData{
-			Name: line[0],
-			Age:  line[1],
-			City: line[2],
-		}
-		fmt.Println(emp.Name + " " + emp.Age + " " + emp.City)
-	}
+	df := dataframe.ReadCSV(csvFile)
+	fmt.Println(df)
+	fil := df.Filter(dataframe.F{
+		Colname:    "age",
+		Comparator: ">",
+		Comparando: 20,
+	})
+	fmt.Println(fil)
 }
