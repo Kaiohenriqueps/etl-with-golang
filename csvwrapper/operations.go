e731c079d8053ff852d5f611e52ba0422e3c797c
Kaiohenriqueps <kh.silva@hotmail.com>
2021-03-09 16:07:58 -0300

    atualizando csv wrapper

--- csvwrapper/operations.go
+++ csvwrapper/operations.go
@@ -1,8 +1,11 @@
 package csvwrapper
 
 import (
+	"etl-with-golang/sqlwrapper"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/kniren/gota/dataframe"
 )
@@ -27,12 +30,24 @@ func FilterFile(path string) {
 		Comparando: 20,
 	})
 
-	SaveAsCsv(fil)
+	rows := fil.Nrow()
+	conn := sqlwrapper.ConnectToPostgres()
+	for i := 0; i < rows; i++ {
+		name := fil.Elem(i, 0).String()
+		age, err := strconv.Atoi(fil.Elem(i, 1).String())
+		if err != nil {
+			log.Fatal(err)
+		}
+		city := fil.Elem(i, 2).String()
+		values := fmt.Sprintf("'%s', %d, '%s'", name, age, city)
+		sqlwrapper.InsertIntoTable(conn, "emp", values)
+	}
+	// SaveAsCsv(fil)
 }
 
 // SaveAsCsv é uma função que salva um dataframe no formato CSV.
 func SaveAsCsv(df dataframe.DataFrame) {
-	f, err := os.Create("data/output.csv")
+	f, err := os.Create("data/output/output.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
